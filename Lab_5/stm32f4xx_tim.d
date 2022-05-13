22af868cc1c1c94d02e8ea79addd0652e3ec40e2
Kotkov Vladislav <kotkov.vv@edu.spbstu.ru>
2022-05-13 14:58:24 +0300

    is done

--- Lab_5/stm32f4xx_tim.d
+++ Lab_5/stm32f4xx_tim.d
@@ -1,11 +1,11 @@
 .\stm32f4xx_tim.o: STM32F4xx_StdPeriph_Driver\src\stm32f4xx_tim.c
 .\stm32f4xx_tim.o: .\STM32F4xx_StdPeriph_Driver\inc\stm32f4xx_tim.h
 .\stm32f4xx_tim.o: .\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
-.\stm32f4xx_tim.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cm4.h
-.\stm32f4xx_tim.o: C:\Program Files\Ukeil\ARM\ARMCC\bin\..\include\stdint.h
-.\stm32f4xx_tim.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cmInstr.h
-.\stm32f4xx_tim.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cmFunc.h
-.\stm32f4xx_tim.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cm4_simd.h
+.\stm32f4xx_tim.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+.\stm32f4xx_tim.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\stm32f4xx_tim.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\stm32f4xx_tim.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\stm32f4xx_tim.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
 .\stm32f4xx_tim.o: .\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
 .\stm32f4xx_tim.o: .\STM32F4xx_StdPeriph_Driver\stm32f4xx_conf.h
 .\stm32f4xx_tim.o: .\STM32F4xx_StdPeriph_Driver\inc\stm32f4xx_gpio.h
--- Lab_5/usart.d
+++ Lab_5/usart.d
@@ -1,10 +1,10 @@
 .\usart.o: Main\Usart.c
 .\usart.o: .\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
-.\usart.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cm4.h
-.\usart.o: C:\Program Files\Ukeil\ARM\ARMCC\bin\..\include\stdint.h
-.\usart.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cmInstr.h
-.\usart.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cmFunc.h
-.\usart.o: C:\Program Files\Ukeil\ARM\CMSIS\Include\core_cm4_simd.h
+.\usart.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
+.\usart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\usart.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\usart.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\usart.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
 .\usart.o: .\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
 .\usart.o: .\STM32F4xx_StdPeriph_Driver\stm32f4xx_conf.h
 .\usart.o: .\STM32F4xx_StdPeriph_Driver\inc\stm32f4xx_gpio.h
